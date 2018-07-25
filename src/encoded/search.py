8a2a7358f3a00661931e84dfbca6d5aeb8aa977d
caseylitton <caseylitton@gmail.com>
2018-07-25 16:25:38 -0700

    doc types func update

--- src/encoded/search.py
+++ src/encoded/search.py
@@ -851,8 +851,8 @@ def search(context, request, search_type=None, return_generator=False):
     print(doc_types, new_doc_types)
     print(result['clear_filters'] == result_clear_filters)
     print(result['clear_filters'], result_clear_filters)
-    print(result['result_filters'] == result_filters)
-    print(result['result_filters'], result_clear_filters)
+    print(result['filters'] == result_filters)
+    print(result['filters'], result_clear_filters)
 
     search_fields, highlights = get_search_fields(request, doc_types)
 
