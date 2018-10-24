b2596d51fa719c0407333315cb92721e4b229a82
Idan Gabdank <gabdank@stanford.edu>
2018-10-24 10:02:52 -0700

    updated the replicate embedding and matrix

--- src/encoded/types/experiment.py
+++ src/encoded/types/experiment.py
@@ -463,7 +463,7 @@ class Experiment(Dataset,
     matrix = {
         'y': {
             'facets': [
-                'replicates.library.biosample.donor.organism.scientific_name',
+                'replicates.libraries.biosample.donor.organism.scientific_name',
                 'biosample_type',
                 'organ_slims',
                 'cell_slims',
@@ -492,7 +492,7 @@ class Experiment(Dataset,
     summary_data = {
         'y': {
             'facets': [
-                'replicates.library.biosample.donor.organism.scientific_name',
+                'replicates.libraries.biosample.donor.organism.scientific_name',
                 'biosample_type',
                 'organ_slims',
                 'cell_slims',
@@ -540,9 +540,14 @@ class Replicate(Item):
         'library.biosample',
         'library.biosample.donor',
         'library.biosample.donor.organism',
+        'libraries',
+        'libraries.biosample',
+        'libraries.biosample.donor',
+        'libraries.biosample.donor.organism',
     ]
     set_status_up = [
         'library',
+        'libraries',
         'antibody',
     ]
     set_status_down = []
